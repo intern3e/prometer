ec1e9570ce2cecb636eb0cefea2ad289f08469af
unknown <devintern3e@gmail.com>
2025-09-29 16:12:54 +0700

    admin1

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -25,8 +25,7 @@ class LoginController extends Controller
     /* -------------------- helper เติม config runtime -------------------- */
     private function bootProvider(string $provider, array $config, string $callbackPath): void
     {
-        // $redirect = 'http://prometer.shop' . $callbackPath;
-         $redirect = 'http://127.0.0.1:8000' . $callbackPath;
+        $redirect = 'http://prometer.shop' . $callbackPath;
         \Log::info("OAuth redirect for {$provider} => {$redirect}");
         config(["services.$provider" => array_merge($config, ['redirect' => $redirect])]);
     }
@@ -37,8 +36,7 @@ class LoginController extends Controller
             'client_id'     => config('services.google.client_id'),
             'client_secret' => config('services.google.client_secret'),
             // บังคับเป็น http://prometer.shop ตอนนี้
-            // 'redirect'      => 'http://prometer.shop/auth/google/callback',
-             'redirect'      => 'http://127.0.0.1:8000/auth/google/callback',
+            'redirect'      => 'http://prometer.shop/auth/google/callback',
             
         ];
         config(['services.google' => $cfg]);
