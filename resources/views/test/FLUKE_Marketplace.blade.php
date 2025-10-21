f8f32b6bf3385dfe36ccf7cecf7d19c75510c2ac
unknown <devintern3e@gmail.com>
2025-10-21 17:42:08 +0700

    test

--- resources/views/test/FLUKE_Marketplace.blade.php
+++ resources/views/test/FLUKE_Marketplace.blade.php
@@ -3,6 +3,7 @@
 <head>
 <title>FLUKE Marketplace Style</title>
 <link rel="icon" type="image/png" href="https://img5.pic.in.th/file/secure-sv1/ChatGPT_Image_18_.._2568_12_03_57-removebg-preview.png">
+<meta name="google-site-verification" content="tpmhTAxPUzD7bh4163L-tqG21SpSCOJC_N0nNxfNt3k" />
 </head>
 <body>
   {{-- Header --}}
